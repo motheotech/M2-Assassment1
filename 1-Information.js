e966b5d07ede6510caadd61f10666343e9a9b68b
motheotech <81598687+motheotech@users.noreply.github.com>
2022-08-23 21:08:30 +0200

    Create 1-Information.js

---
+++ 1-Information.js
@@ -0,0 +1,5 @@
+const Names = "Naledi"
+const Institution = "WITS"
+const Github_username = "Lennas_"
+var statement ="My name is "+Names+", a student from "+Institution+" and my Github username is "+ Github_username;
+console.log(statement);
