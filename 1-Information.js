ffb8074c8a058e4163ff39febadb659b404a6c9d
motheotech <81598687+motheotech@users.noreply.github.com>
2022-08-25 02:35:43 +0200

    Update 1-Information.js

--- 1-Information.js
+++ 1-Information.js
@@ -1,5 +1,5 @@
-const Names = "Naledi"
-const Institution = "WITS"
-const Github_username = "Lennas_"
+const Names = window.prompt("Enter your Name")
+const Institution = window.prompt("Enter your Surname")
+const Github_username = window.prompt("Enter your Github_Username")
 var statement ="My name is "+Names+", a student from "+Institution+" and my Github username is "+ Github_username;
 console.log(statement);
